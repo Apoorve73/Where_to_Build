04858424a8de31cef8154a66064bfca441242481
Apoorve73 <apoorve73@gmail.com>
2020-03-31 20:48:13 +0530

    Reorganise codebas

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ lon1 = list()
 lat2 = list()
 lon2 = list()
 add = list()
+img = list()
 
 # Assigning variables, the keys for latitude and longitude in json file
 lat = 'latitude'
@@ -39,7 +40,7 @@ for properties in properties_json:
     lat1.append(properties[lat])
     lon1.append(properties[lon])
     add.append(properties[prop_address][title])
-    ##print(lat1[i] , lon1[i])
+    img.append(properties['imageUrl'])
     i = i + 1
 print("----------------------------------------------------------------------------------------------------------")
 i = 0
@@ -51,21 +52,21 @@ for properties in properties_json2:
 
 # Calculating distance between land on sale and nearby hospital using standard formulae based on Latitude and Longitude
 def degreesToRadians(degrees):
-    return degrees * Math.pi / 180;
+    return degrees * Math.pi / 180
 
 
 def distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2):
-    earthRadiuskm = 6371;
+    earthRadiuskm = 6371
 
-    dLat = degreesToRadians(lat2 - lat1);
-    dLon = degreesToRadians(lon2 - lon1);
+    dLat = degreesToRadians(lat2 - lat1)
+    dLon = degreesToRadians(lon2 - lon1)
 
-    lat1 = degreesToRadians(lat1);
-    lat2 = degreesToRadians(lat2);
+    lat1 = degreesToRadians(lat1)
+    lat2 = degreesToRadians(lat2)
 
     a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(
-        lat2);
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        lat2)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
     return (earthRadiuskm * c)
 
 distance = list()
@@ -73,6 +74,7 @@ latitude = list()
 longitude = list()
 num_Hosp = list()
 address = list()
+imageurl = list()
 
 # def in_range_check(latt, lont, lath, lonh, count, j):
 #     #for j in range(0,len(lon1)):
@@ -91,19 +93,22 @@ for i in range(0, len(lat1)):
         # Selecting the Property having a hospital in the range of 3Km
         if (dis <= 1):
             count = count + 1  # Counting number of such hospitals
-    ##count = in_range_check(lat1[i], lon1[i], lat2, lon2, 0, 0)
 
     if count > 0:  # Check for whether the given property has at least one hospital in the given range
         latitude.append(lat1[i])
         longitude.append(lon1[i])
         address.append(add[i])
+        imageurl.append(img[i])
         num_Hosp.append(count)
     distance.clear()
 
 coordinates = list()
 for i in range(len(address)):
-    coordinates.append({'address': address[i], 'lat': latitude[i], 'lng': longitude[i]})
+    coordinates.append({'address': address[i], 'lat': latitude[i], 'lng': longitude[i], 'imageurl' : imageurl[i]})
 
+print("-----------------------------------------------------------------------------------------------------------")
+
+# Authentication Starts
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -139,26 +144,6 @@ class RegisterForm(FlaskForm):
 
 
 db.create_all()
-        #{'lat' : '32.779167', 'lng' : '-96.808891'}]
-@app.route("/")
-
-def home():
-    return render_template('index_main.html')
-
-@app.route("/about")
-
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-
-def contacts():
-    return render_template('contact.html')
-
-@app.route("/services")
-#decorator, just a way to add functionality to ezisting function
-def services():
-    return render_template('services.html', coords = coordinates)
 
 @app.route('/Redirect')
 def index():
@@ -210,9 +195,29 @@ def logout():
     logout_user()
     return redirect(url_for('Redirect'))
 
+#Authentication Ends
 
+# Routes for webpage
+@app.route("/")
+
+def home():
+    return render_template('index_main.html')
+
+@app.route("/about")
+
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+
+def contacts():
+    return render_template('contact.html')
+
+# Map integration with the web app
+@app.route("/services")
+#decorator, just a way to add functionality to ezisting function
+def services():
+    return render_template('services.html', coords = coordinates)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
