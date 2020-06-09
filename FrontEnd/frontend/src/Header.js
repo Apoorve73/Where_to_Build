ce8dca92e9d80f0741aee6f08aeec7fdb7cab597
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-10 01:03:42 +0530

    Remove Header component

---
+++ FrontEnd/frontend/src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Navbar from "./Navbar";
+
+const Header = () => {
+    return(
+        <div>
+            <h1>This is header</h1>
+        </div>
+        
+    );
+}
+
+export default Header;
