020f71a59ec7a71e7f9204af9c76b9a46b11a51e
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-10 00:56:39 +0530

    Add NavbabarTiles Component

---
+++ FrontEnd/frontend/src/NavbarTiles.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const NavbarTiles = (props) => {
+    return(
+        <div>
+            
+                { /*eslint-disable-next-line */} 
+                <li><a href="#">{props.chore}<span></span><span></span></a></li>
+            
+        </div>
+    );
+}
+
+export default NavbarTiles;
