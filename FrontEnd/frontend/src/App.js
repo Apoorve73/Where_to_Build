7accf298b9b3b6fcac0c6b552f94d89542857529
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-10 01:02:32 +0530

    Add Navbar component

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import Header from './Header';
+//import Footer from './Footer';
+//import MainContent from './MainContent';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
